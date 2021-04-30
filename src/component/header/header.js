2884f664e43c540f0e598376815c0827c7ca5033
MuhammadMuhammad <muhammadsadiqhanif@gmail.com>
2021-04-30 05:55:57 -0700

    fixed some issues

--- src/component/header/header.js
+++ src/component/header/header.js
@@ -6,15 +6,17 @@ function Header() {
     return (
         <div className="HeaderPage">
             <div className="headerMainDiv">
-                <div className="headerSection1">
-                    <img src={Logo}/>
+                <div className="headerSection1 col-2">
+                    <img src={Logo} />
                 </div>
-                <div className="headerSection2">
+                <div className="headerSection2 col-7">
                     <input type="email" className="form-control roundedInput" placeholder="Search for gigs, artist & venues" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                </div>
+                <div className="headerSection2 col-3">
                     <button className="btn btn-warning buttonAddGig"><PlusSvg /> Add Gig</button>
                     <div className="dropdown">
                         <button className="btn d-flex btn-light dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                           <div className="profileDiv"></div>  Hi, Tam
+                            <div className="profileDiv"></div>  Hi, Tam
                         </button>
                         <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
                             <a className="dropdown-item" href="#">Action</a>
